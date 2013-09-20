9b40ad26f48346ff4926fc19aaea6cd541714f74
Vltr <valtteri.makela@iki.fi>
2013-09-20 13:01:33 +0300

    projektirunko

---
+++ bibikset/src/test/java/fi/helsinki/TestiTesti.java
@@ -0,0 +1,17 @@
+package fi.helsinki.jkmv;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import static org.junit.Assert.*;
+
+public class TestiTesti {
+	
+	@Test
+	public void testTest() {
+		assertEquals(7, 7);
+	}
+}
